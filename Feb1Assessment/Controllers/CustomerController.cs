83990e879595e030bd6deec5f2c629d3c0594f73
Niteshvarmaa <niteshvarma126@gmail.com>
2024-02-01 21:47:20 +0530

    Add project files.

---
+++ Feb1Assessment/Controllers/CustomerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Feb1Assessment.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            List<string> list = new List<string>()
+            {
+                "raja",
+                "Surya"
+            };
+            ViewBag.namelist = list;
+            return View();
+        }
+        public ActionResult ReDirectDemo()
+        {
+            TempData["tempmsg"] = "Welcome to our page";
+            return RedirectToAction("DisplayMessage");
+        }
+        public ActionResult DisplayMessage()
+        {
+
+            return View();
+        }
+    }
+}
